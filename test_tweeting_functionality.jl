e578129550fc575bc095d7627927029b3410f885
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2019-11-11 15:43:04 +0100

    minor changes

--- test_tweeting_functionality.jl
+++ test_tweeting_functionality.jl
@@ -3,13 +3,14 @@ include("Tweet.jl")
 include("Network.jl")
 include("Simulation.jl")
 
+# --- test update functions --- # 
 
 g = create_network(100, 3, 1)
 a = create_agents(g)
 
-# update_perceiv_publ_opinion!(g, 4, a)
-# update_opinion!(a, 4)
-# update_inclin_interact!(a, 4)
+update_perceiv_publ_opinion!(g, 4, a)
+update_opinion!(a, 4)
+update_inclin_interact!(a, 4)
 
 push!(a[1].timeline, Tweet(0.5, 0))
 push!(a[1].timeline, Tweet(0.3, 17))
@@ -17,6 +18,8 @@ push!(a[1].timeline, Tweet(0.3, 17))
 publish_tweet!(a, g, 4)
 update_timeline!(a, 1, 0.5)
 
+
+# --- test simulation --- #
 
 g = create_network(100, 3, 1)
 a = create_agents(g)
